c221f4e1a6dc30fe63bd137a4ffb308a7862d542
Elmir9555 <elmiriu@code.edu.az>
2022-06-03 14:08:06 +0400

    Start of BackendProject

---
+++ EndProjectOrgani/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EndProjectOrgani.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
